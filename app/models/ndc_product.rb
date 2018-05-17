1afef5158e83bb0b3e6a863732083cf0f1adbe01
Jack Reuter <djreuter45@gmail.com>
2018-05-17 16:01:49 -0600

    modified query to return one result per name

--- app/models/ndc_product.rb
+++ app/models/ndc_product.rb
@@ -1,5 +1,5 @@
 class NdcProduct < ApplicationRecord
   def self.search(search)
-    where('proprietary_name LIKE ?', "%#{search}%")
+    where('proprietary_name LIKE ?', "%#{search}%").select('DISTINCT(ndc_products.proprietary_name)').order(:proprietary_name)
   end
 end

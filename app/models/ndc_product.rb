ac24aa780c4b42a51edf4e3f0470299aa466766c
Jack Reuter <djreuter45@gmail.com>
2018-05-21 14:08:07 -0600

    modified query for distinct dosage_form_name

--- app/models/ndc_product.rb
+++ app/models/ndc_product.rb
@@ -4,6 +4,6 @@ class NdcProduct < ApplicationRecord
   end
 
   def self.find_drug(search)
-    where('proprietary_name LIKE ? OR non_proprietary_name LIKE ?', "%#{search}%", "%#{search}%").select('DISTINCT(ndc_products.dosage_form_name), (ndc_products.active_numerator_strength), (ndc_products.active_ingred_unit)').order(:dosage_form_name)
+    where('proprietary_name LIKE ? OR non_proprietary_name LIKE ?', "%#{search}%", "%#{search}%").select('DISTINCT(ndc_products.dosage_form_name)').order(:dosage_form_name)
   end
 end

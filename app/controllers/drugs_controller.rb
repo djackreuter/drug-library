cd60d64bff0556de21281546e5c81c2ba265d5d6
Jack Reuter <djreuter45@gmail.com>
2018-05-30 10:15:48 -0600

    commented out unused queries and added a check to not query empty string

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,18 +1,21 @@
 class DrugsController < ApplicationController
   def query
+    if search_params[:search] == ''
+      raise ArgumentError, 'No search params specified'
+    end
     drugs = NdcProduct.search(search_params[:search].capitalize)
     render json: drugs, status: :ok
   end
 
-  def specific_drug
-    drug = NdcProduct.find_drug(search_params[:product_id])
-    render json: drug, status: :ok
-  end
+  # def specific_drug
+  #   drug = NdcProduct.find_drug(search_params[:product_id])
+  #   render json: drug, status: :ok
+  # end
 
-  def dosage_strength
-    drug = NdcProduct.get_strength(search_params[:search].capitalize, search_params[:dose_form])
-    render json: drug, status: :ok
-  end
+  # def dosage_strength
+  #   drug = NdcProduct.get_strength(search_params[:search].capitalize, search_params[:dose_form])
+  #   render json: drug, status: :ok
+  # end
 
   private
 
--- app/models/ndc_product.rb
+++ app/models/ndc_product.rb
@@ -3,11 +3,11 @@ class NdcProduct < ApplicationRecord
     where('proprietary_name LIKE ? OR non_proprietary_name LIKE ?', "#{search}%", "#{search}%")
   end
 
-  def self.find_drug(product_id)
-    find_by('product_id=?', "#{product_id}")
-  end
+  # def self.find_drug(product_id)
+  #   find_by('product_id=?', "#{product_id}")
+  # end
 
-  def self.get_strength(search, dose_form)
-    where('proprietary_name LIKE ? OR non_proprietary_name LIKE ?', "%#{search}%", "%#{search}%").where(dosage_form_name: dose_form).pluck(:active_numerator_strength, :active_ingred_unit).uniq
-  end
+  # def self.get_strength(search, dose_form)
+  #   where('proprietary_name LIKE ? OR non_proprietary_name LIKE ?', "%#{search}%", "%#{search}%").where(dosage_form_name: dose_form).pluck(:active_numerator_strength, :active_ingred_unit).uniq
+  # end
 end
